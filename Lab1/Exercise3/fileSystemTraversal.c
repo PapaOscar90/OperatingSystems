47eac13a1489a94dbf379a394cb7c5507d1d53c2
Philip Oetinger <p.oetinger@student.rug.nl>
2019-02-15 17:55:43 +0100

    adds basic logic to test all elements in array

--- Lab1/Exercise3/fileSystemTraversal.c
+++ Lab1/Exercise3/fileSystemTraversal.c
@@ -18,15 +18,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+int isSameFile(int file1, int file2);
+
 int main(){
     char *lineIn;
     size_t lineLen;
 
-    lineLen = getline(&lineIn, &lineLen, stdin);
-    lineIn[lineLen-1] = '\0';
-    lineLen--;
+    int numberOfFiles = 5;
+    int files[5] = {0,0,0,0,0};
 
-    printf("%lu\n", lineLen);
-    printf("%s\n", lineIn);
+    // Check every element to see if it matches another element in the array
+    for(int i=0; i < numberOfFiles; i++){
+        for(int j = (i+1); j < numberOfFiles; j++){
+            if(isSameFile(files[i], files[j])){
+                printf("%d and %d are the same file\n", i, j);
+            }
+        }
+    }
+
+    
     return 0;
+}
+
+
+int isSameFile(int file1, int file2){
+    // TODO: Syscalls
+    return 1;
 }
