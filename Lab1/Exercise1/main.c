9e2bb272037e85941fe4b7dbc6dd38e79ba987ab
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-02-19 15:28:37 +0100

    Cleanup exercise 1

--- Lab1/Exercise1/main.c
+++ Lab1/Exercise1/main.c
@@ -4,9 +4,9 @@
  * Troy Karan Harrison - s2841886
  * Philip Oetinger     - s2966018
  *
- * This program takes in a command (optional quotes) with arguments
- * and runs a new process that executes the command with arguments
- *
+ * This program reads a command (optional contained within quotes) and optional
+ * arguments from stdin and runs a new process that executes the command with
+ * its arguments
  */
 
 #include "util.h"
@@ -15,100 +15,196 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-#ifndef PATH_MAX
-#define PATH_MAX 4096
+#ifndef LINE_MAX
+#define LINE_MAX 4096
 #endif
 
-#ifndef FILENAME_MAX
-#define FILENAME_MAX 256
+// NOTE 127 is the exit code returned by bash when a command is not
+// found on the user's path and the command is not a built-in
+// shell command
+#ifndef EXIT_COMMAND_NOT_FOUND
+#define EXIT_COMMAND_NOT_FOUND 127
 #endif
 
-// This function will find and seperate the command from the rest of the arguments
-void seperateCommandFromArguments(char* input, char* command, char* arguments){
-  // If the command starts with ", look for the closing and make that the command
-  if (input[0] == '"') {
-    int secondQuote = 1;
+// Parse a command from an input string.
+//   A command is either the first consecutive string that contains no
+//   non-line-breaking whitespace characters
+//   OR
+//   The text enclosed between the first pair of single or double quotes.
+char *parseCommand(const char *input) {
+  size_t i = 0;
+  size_t inputLen = strlen(input);
+  size_t startOfCommand;
+  size_t endOfCommand;
+  // Skip whitespace
+  while (i < inputLen && isNonBreakingSpace(input[i]))
+    i++;
 
-    while (input[secondQuote] != '"') {
-      secondQuote++;
+  // If the command starts with a double quote, search for the closing quote
+  if (input[i] == '"' || input[i] == '\'') {
+    size_t firstQuoteIdx = i;
+    size_t secondQuoteIdx = i + 1;
+    // Find the position of the second quote
+    while (secondQuoteIdx < inputLen &&
+           input[secondQuoteIdx] != input[firstQuoteIdx]) {
+      secondQuoteIdx++;
     }
 
-    // Now copy the command and arguments to their respective strings
-    strncpy(command, input + 1, secondQuote - 1);
-    strncpy(arguments, input + secondQuote + 2, PATH_MAX);
-  } else {
-    // If no quotes used, find the next ' ' signifying end of command
-    int endOfCommand = 1;
+    // If no matching quote was found
+    if (secondQuoteIdx >= inputLen) {
+      fprintf(stderr, "Mismatched quotation: %c\n", input[firstQuoteIdx]);
+      exit(EXIT_FAILURE);
+    }
 
-    while (input[endOfCommand] != ' ') {
+    // Enclosed is the command sans the quotes
+    startOfCommand = firstQuoteIdx + 1;
+    endOfCommand = secondQuoteIdx;
+  } else {
+    // As there are no quotes the end of the command is denoted by the first
+    // whitespace character
+    startOfCommand = i;
+    endOfCommand = i + 1;
+
+    // Increment till a whitespace character is found
+    while (endOfCommand < inputLen &&
+           !isNonBreakingSpace(input[endOfCommand])) {
       endOfCommand++;
     }
+  }
 
-    // Now copy the command and arguments to their respective strings
-    strncpy(command, input, endOfCommand);
-    strncpy(arguments, input + endOfCommand + 1, PATH_MAX);
+  // Copy the command from the line and return it
+  char *command =
+      safeCalloc(endOfCommand - startOfCommand + 1, sizeof(*command));
+  strncpy(command, input + startOfCommand, endOfCommand - startOfCommand);
+  return command;
+}
+
+// Extract the arguments from an input string.
+char *parseArguments(const char *input) {
+  size_t i = 0;
+  size_t inputLen = strlen(input);
+
+  // Skip whitespace
+  while (i < inputLen && isNonBreakingSpace(input[i]))
+    i++;
+
+  // Skip past the argument
+  if (input[i] == '"' || input[i] == '\'') {
+    // Skip past the quotes if they exist
+    size_t firstQuoteIdx = i;
+    i++;
+    while (i < inputLen && input[i] != input[firstQuoteIdx])
+      i++;
+  } else {
+    // Skip past the text of the argument
+    while (i < inputLen && !isNonBreakingSpace(input[i]))
+      i++;
+  }
+
+  // Skip whitespace before the arguments
+  while (i < inputLen && isNonBreakingSpace(input[i]))
+    i++;
+
+  if (i < inputLen) {
+    // If there are arguments, copy them
+    char *arguments = safeCalloc(strlen(input) - i + 1, sizeof(*arguments));
+    strncpy(arguments, input + i, strlen(input) - i);
+    return arguments;
+  } else {
+    // There are no arguments
+    return NULL;
   }
 }
 
-// Go through all the paths from getenv to check if cmd exists
-void findFilePath(char* allPaths, char* command, char* fullFilePath){
-  struct stat buffer;
-  int exists = 1;
+// Go through the user's path from getenv to check if command exists
+char *findCommandPath(char *command) {
+  // Get the user's path
+  char *path = getenv("PATH");
 
-  char *token = strtok(allPaths, ":");
-  // Go through all the paths, and if a command is found, break with fullFilePath containing path
-  while (token != NULL ){
-    sprintf(fullFilePath, "%s/%s", token, command);
-    exists = stat(fullFilePath, &buffer);
-    if (exists == 0 && S_ISREG(buffer.st_mode)){
-      break;
+  char *absolutePath;
+
+  struct stat buffer;
+
+  char *token = strtok(path, ":");
+  // Go through all the paths appending the command.
+  //   If the path exists, then break returning that path.
+  while (token != NULL) {
+    absolutePath =
+        safeCalloc(strlen(token) + strlen(command) + 2, sizeof(*absolutePath));
+    // Append the command to the directory
+    sprintf(absolutePath, "%s/%s", token, command);
+
+    // If that path exists and is a regular file.
+    if (stat(absolutePath, &buffer) == 0 && S_ISREG(buffer.st_mode)) {
+      return absolutePath;
     }
 
-    token = strtok(NULL,":");
+    // Get the next directory from the path
+    token = strtok(NULL, ":");
+    // Cleanup
+    free(absolutePath);
   }
+
+  // The command is not in the user's path
+  return NULL;
 }
 
-
 int main() {
-  char line[PATH_MAX];
-  char execCmd[FILENAME_MAX];
-  char arguments[PATH_MAX];
-  int child, status;
+  // Read the commands and arguments into a string from stdin
+  char *line = readLine(LINE_MAX, stdin);
 
-  readLine(line, PATH_MAX, stdin);
+  // Parse the command from the user input
+  char *command = parseCommand(line);
+  // Parse the arguments from the user input
+  char *arguments = parseArguments(line);
 
-  seperateCommandFromArguments(line, execCmd, arguments);
-
-  char *allPaths = getenv("PATH");
-  char fullFilePath[PATH_MAX];
-  findFilePath(allPaths, execCmd, fullFilePath);
+  // Find the command's absolute path
+  char *commandPath = findCommandPath(command);
 
   // Create a child process that will execute the command
+  int child;
   child = fork();
   if (child < 0) {
     fprintf(stderr, "Fork failed: aborted\n");
     return EXIT_FAILURE;
   }
 
-
-  if (child != 0) {
-    /* Parent runs nothing */
-  } else {
-    /* Child runs the new core with arguments that were passed in */
+  if (child == 0) {
+    // Child runs the new core with the command the provided arguments
     char *newargv[3];
-    newargv[0] = execCmd;
+    newargv[0] = command;
     newargv[1] = arguments;
     newargv[2] = NULL;
 
-    execve(fullFilePath, newargv, NULL);
-    printf("Command %s not found!\n", execCmd);
-    exit(EXIT_FAILURE);
+    execve(commandPath, newargv, NULL);
+
+    // If the command could not be executed
+
+    printf("Command %s not found!\n", command);
+
+    // Cleanup
+    free(line);
+    free(command);
+    free(arguments);
+    free(commandPath);
+
+    return EXIT_COMMAND_NOT_FOUND;
   }
+
+  // Wait for the child to report its status
+  int status;
   waitpid(-1, &status, 0);
 
-  return EXIT_SUCCESS;
+  // Cleanup
+  free(line);
+  free(command);
+  free(arguments);
+  free(commandPath);
+
+  // Forward the exit status of the child
+  return WEXITSTATUS(status);
 }
--- Lab1/Exercise1/util.h
+++ Lab1/Exercise1/util.h
@@ -5,17 +5,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-int readLine(char *str, int n, FILE *stream) {
+// calloc which panics on OOM.
+void *safeCalloc(size_t __nmemb, size_t __size) {
+  void *p = calloc(__nmemb, __size);
+  if (p == NULL) {
+    fprintf(stderr, "Calloc failed. Out of memory?\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return p;
+}
+
+int isNonBreakingSpace(char c) { return c == ' ' || c == '\t'; }
+
+// Read at most n characters from a line from a provided file stream.
+char *readLine(int n, FILE *stream) {
+  char *str = safeCalloc(n, sizeof(*str));
   char *ptr = fgets(str, n, stream);
   if (ptr == NULL) {
-    // TODO print the name of the stream in the error.
     fprintf(stderr, "Error when reading line.\n");
     exit(EXIT_FAILURE);
   }
-  int strLength = strlen(str);
-  strLength--;
-  str[strLength] = '\0';
-  return strLength;
+
+  // Replace the trailing newline (if it exists) with a null character
+  int strLen = strlen(str);
+  strLen--;
+  if (str[strLen] == '\n')
+    str[strLen] = '\0';
+
+  // Truncate the memory of the temporary string and store the result in line.
+  char *line = safeCalloc(strLen + 1, sizeof(*line));
+  strncpy(line, str, strLen);
+
+  // Cleanup
+  free(str);
+
+  return line;
 }
 
 #endif /* UTIL_H */
