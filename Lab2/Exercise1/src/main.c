5f0fd601c387bb230400cd35d20ebe2f2f31c9f7
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-03-14 12:52:27 +0100

    Add frame for reading a command

--- Lab2/Exercise1/src/main.c
+++ Lab2/Exercise1/src/main.c
@@ -6,11 +6,24 @@
 // NOTE this could be extended to allow for user customization.
 void typePrompt(void) { printf("$ "); }
 
+// TODO Read a line from a file stream
+char *readLine(FILE *stream) {
+  char c;
+  fscanf(stream, "%c\n", &c);
+
+  fprintf(stderr, "Unimplemented\n");
+  exit(EXIT_FAILURE);
+}
+
+// TODO Read the command from the terminal
+void readCommand() { char *input = readLine(stdin); }
+
 // The shell follows the basic structure as provided in the Minix book:
 int main() {
 
   while (true) {  // repeat forever
     typePrompt(); // display prompt on the screen
+    readCommand();
   }
 
   return EXIT_SUCCESS;
