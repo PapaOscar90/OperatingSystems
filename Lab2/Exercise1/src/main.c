84295a9395addbb2547880a25be13df5ecb2dbaf
Troy Karan Harrison <NoxPardalis@protonmail.com>
2019-03-14 12:31:20 +0100

    Add prompt

--- Lab2/Exercise1/src/main.c
+++ Lab2/Exercise1/src/main.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Display the shell prompt to the user.
+// NOTE this could be extended to allow for user customization.
+void typePrompt(void) { printf("$ "); }
+
 // The shell follows the basic structure as provided in the Minix book:
 int main() {
 
-  while (true) { // repeat forever
+  while (true) {  // repeat forever
+    typePrompt(); // display prompt on the screen
   }
 
   return EXIT_SUCCESS;
