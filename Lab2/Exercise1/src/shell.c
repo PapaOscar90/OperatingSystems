41fd70e31ee9bad6a0e085fc8949ffb87f35611d
Philip Oetinger <p.oetinger@student.rug.nl>
2019-03-18 10:03:02 +0100

    Update the README

--- Lab2/Exercise1/src/shell.c
+++ Lab2/Exercise1/src/shell.c
@@ -7,12 +7,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void eval(char const *eval_string) {
+#ifndef LINE_MAX
+#define LINE_MAX 10000000
+#endif
+
+void eval(char const *eval_string)
+{
   size_t num_commands = 0;
   Command *commands = parse(eval_string, &num_commands);
 
   // For each command in commands
-  for (size_t i = 0; i < num_commands; ++i) {
+  for (size_t i = 0; i < num_commands; ++i)
+  {
     exec(commands[i]);
   }
 
@@ -20,8 +26,10 @@ void eval(char const *eval_string) {
 }
 
 // TODO should this return the exit_code along side the inevitable shell_status?
-void exec(Command command) {
-  switch (command.type) {
+void exec(Command command)
+{
+  switch (command.type)
+  {
   case BUILTIN:
     return exec_builtin(command);
   case EXTERNAL:
@@ -29,12 +37,18 @@ void exec(Command command) {
   }
 }
 
-void exec_builtin(Command command) {
+void exec_builtin(Command command)
+{
   DBG("Executing builtin command...");
   DBG("Command: %u", command.builtin);
   DBG("Arguments: %s", command.arguments);
   DBG("In background?: %s", command.in_background ? "true" : "false");
   DBG("Redirection: %d", command.redirection.type);
+  switch (command.builtin)
+  {
+  case EXIT:
+    break;
+  }
 
   // If in background spawn child to run command.
   // If with redirection spawn fds and pipes to pass in and pass out results.
@@ -42,19 +56,60 @@ void exec_builtin(Command command) {
   // existence. Execute command with arguments
 }
 
-void exec_external(Command command) {
+void exec_external(Command command)
+{
+  // If in background spawn child to run command.
+  // If with redirection spawn fds and pipes to pass in and pass out results.
+  // Check if command is on path. If not return error.
+  // If is on path, execute command with arguments.
   DBG("Executing external command...");
   DBG("Command: %s", command.external);
   DBG("Arguments: %s", command.arguments);
   DBG("In background?: %s", command.in_background ? "true" : "false");
   DBG("Redirection: %d", command.redirection.type);
 
-  // If in background spawn child to run command.
-  // If with redirection spawn fds and pipes to pass in and pass out results.
-  // Check if command is on path. If not return error.
-  // If is on path, execute command with arguments.
+  // Find the command's absolute path
+  char *commandPath = findCommandPath(command.external);
+
+  int child;
+  child = fork();
+  if (child < 0)
+  {
+    fprintf(stderr, "Fork failed: aborted\n");
+    return EXIT_FAILURE;
+  }
+
+  char *commandPath = findCommandPath(command);
+  if (commandPath == NULL)
+  {
+    // If the command could not be found on the user's path
+    printf("Command %s not found!\n", command);
+    free(commandPath);
+    return EXIT_SUCCESS;
+  }
+
+  if (child == 0)
+  {
+    char *newargv[3];
+    newargv[0] = command.external;
+    newargv[1] = command.arguments;
+    newargv[2] = NULL;
+
+    execve(commandPath, command.arguments, NULL);
+  }
+
+  if (!command.in_background)
+  {
+    int status;
+    waitpid(-1, &status, 0);
+  }
+
+  free(commandPath);
+  free(command.arguments);
+  free(command.external);
 }
 
-void type_prompt() {
+void type_prompt()
+{
   // NOTE Currently no prompt displayed.
 }
--- Lab2/Exercise1/src/util.c
+++ Lab2/Exercise1/src/util.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *checked_malloc(size_t size) {
+void *checked_malloc(size_t size)
+{
   void *p = malloc(size);
-  if (p == NULL) {
+  if (p == NULL)
+  {
     fprintf(stderr, "Malloc failed, out of memory?\n");
     exit(EXIT_FAILURE);
   }
@@ -14,9 +16,11 @@ void *checked_malloc(size_t size) {
   return p;
 }
 
-void *checked_calloc(size_t nmemb, size_t size) {
+void *checked_calloc(size_t nmemb, size_t size)
+{
   void *p = calloc(nmemb, size);
-  if (p == NULL) {
+  if (p == NULL)
+  {
     fprintf(stderr, "Calloc failed, out of memory?\n");
     exit(EXIT_FAILURE);
   }
@@ -24,24 +28,30 @@ void *checked_calloc(size_t nmemb, size_t size) {
   return p;
 }
 
-void read_line(char **line, size_t *line_len) {
+void read_line(char **line, size_t *line_len)
+{
   getline(line, line_len, stdin);
   *line_len = strlen(*line) - 1;
   (*line)[*line_len] = '\0';
 }
 
-char *unquote_string(char const *string) {
+char *unquote_string(char const *string)
+{
   char *intermediary = checked_malloc(strlen(string) + 1);
   size_t result_len = 0;
   bool in_quote = false;
   char quote = '\0';
 
-  for (size_t i = 0; i < strlen(string); ++i) {
-    if (in_quote && string[i] == quote) {
+  for (size_t i = 0; i < strlen(string); ++i)
+  {
+    if (in_quote && string[i] == quote)
+    {
       in_quote = false;
       quote = '\0';
       continue;
-    } else if (!in_quote && (string[i] == '\'' || string[i] == '"')) {
+    }
+    else if (!in_quote && (string[i] == '\'' || string[i] == '"'))
+    {
       in_quote = true;
       quote = string[i];
       continue;
@@ -56,10 +66,150 @@ char *unquote_string(char const *string) {
 
   free(intermediary);
 
-  if (in_quote) {
+  if (in_quote)
+  {
     free(result);
     return NULL;
-  } else {
+  }
+  else
+  {
     return result;
   }
+}
+
+// Parse a command from an input string.
+//   A command is either the first consecutive string that contains no
+//   non-line-breaking whitespace characters
+//   OR
+//   The text enclosed between the first pair of single or double quotes.
+char *parseCommand(const char *input)
+{
+  size_t offset = 0;
+  size_t inputLen = strlen(input);
+
+  char *tempCommand = safeCalloc(inputLen + 1, sizeof(*tempCommand));
+
+  // Skip whitespace
+  while (offset < inputLen && isNonBreakingSpace(input[offset]))
+    offset++;
+
+  int withinDoubleQuotes = 0;
+  int withinSingleQuotes = 0;
+  size_t i = 0;
+  while (offset < inputLen && (withinDoubleQuotes || withinSingleQuotes ||
+                               !isNonBreakingSpace(input[offset])))
+  {
+    if (!withinSingleQuotes && input[offset] == '"')
+    {
+      withinDoubleQuotes = !withinDoubleQuotes;
+    }
+    else if (!withinDoubleQuotes && input[offset] == '\'')
+    {
+      withinSingleQuotes = !withinSingleQuotes;
+    }
+    else
+    {
+      tempCommand[i] = input[offset];
+      i++;
+    }
+    offset++;
+  }
+
+  if (withinDoubleQuotes || withinSingleQuotes)
+  {
+    free(tempCommand);
+    fprintf(stderr, "Mismatched quotation: %c\n",
+            withinDoubleQuotes ? '"' : '\'');
+    exit(EXIT_FAILURE);
+  }
+
+  tempCommand[offset] = '\0';
+
+  // Copy the command from the temporary buffer and return it
+  char *command = safeCalloc(strlen(tempCommand) + 1, sizeof(*command));
+  strcpy(command, tempCommand);
+  free(tempCommand);
+  return command;
+}
+
+// Extract the arguments from an input string.
+char *parseArguments(const char *input)
+{
+  size_t offset = 0;
+  size_t inputLen = strlen(input);
+
+  // Skip whitespace
+  while (offset < inputLen && isNonBreakingSpace(input[offset]))
+    offset++;
+
+  // Skip past the argument
+  int withinDoubleQuotes = 0;
+  int withinSingleQuotes = 0;
+  while (offset < inputLen && (withinDoubleQuotes || withinSingleQuotes ||
+                               !isNonBreakingSpace(input[offset])))
+  {
+    if (!withinSingleQuotes && input[offset] == '"')
+    {
+      withinDoubleQuotes = !withinDoubleQuotes;
+    }
+    else if (!withinDoubleQuotes && input[offset] == '\'')
+    {
+      withinSingleQuotes = !withinSingleQuotes;
+    }
+    offset++;
+  }
+
+  // Skip whitespace before the arguments
+  while (offset < inputLen && isNonBreakingSpace(input[offset]))
+    offset++;
+
+  if (offset < inputLen)
+  {
+    // If there are arguments, copy them
+    char *arguments =
+        safeCalloc(strlen(input) - offset + 1, sizeof(*arguments));
+    strncpy(arguments, input + offset, strlen(input) - offset);
+    return arguments;
+  }
+  else
+  {
+    // There are no arguments
+    return NULL;
+  }
+}
+
+// Go through the user's path from getenv to check if command exists
+char *findCommandPath(char *command)
+{
+  // Get the user's path
+  char *path = getenv("PATH");
+
+  char *absolutePath;
+
+  struct stat buffer;
+
+  char *token = strtok(path, ":");
+  // Go through all the paths appending the command.
+  //   If the path exists, then break returning that path.
+  while (token != NULL)
+  {
+    absolutePath =
+        safeCalloc(strlen(token) + strlen(command) + 2, sizeof(*absolutePath));
+    // Append the command to the directory
+    sprintf(absolutePath, "%s/%s", token, command);
+
+    // If that path exists and is a regular file.
+    if (stat(absolutePath, &buffer) == 0 && S_ISREG(buffer.st_mode))
+    {
+      return absolutePath;
+    }
+
+    // Get the next directory from the path
+    token = strtok(NULL, ":");
+    // Cleanup
+    free(absolutePath);
+  }
+
+  // The command is not in the user's path
+  return NULL;
 }
