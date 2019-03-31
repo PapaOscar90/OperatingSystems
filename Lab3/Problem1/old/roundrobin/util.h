278809d7b478c6e2952d8484a368b34782acad60
Philip Oetinger <p.oetinger@student.rug.nl>
2019-03-31 17:19:36 +0200

    Add old

---
+++ Lab3/Problem1/old/roundrobin/util.h
@@ -0,0 +1,56 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+#include "process.h"
+#include "scheduler.h"
+
+void printArrays(int *array, int length)
+{
+    printf("[");
+    for (int i = 0; i < length - 1; i++)
+    {
+        printf("%d,", array[i]);
+    }
+    printf("%d]", array[length - 1]);
+}
+
+// Debug printing of process
+void printProcess(Process testProcess)
+{
+    printf("Process StartTime: %d, EndTime: %d\n", testProcess.startTime, testProcess.endTime);
+    printf("Process CPUq: ");
+    printArrays(testProcess.CPUq, testProcess.sizeCPUq);
+    printf("\nProcess IOq: ");
+    printArrays(testProcess.IOq, testProcess.sizeIOq);
+    printf("\n-------------------------------\n");
+}
+
+// Debug printing of a scheduler and it's contents
+void printScheduler(Scheduler scheduler)
+{
+    printf("\nScheduler (Q=%d) Contains:\n# Standby: %d\n", scheduler.quantum, scheduler.numStandby);
+    printf("Priority1 (%d):\n---------------------\n", scheduler.numP1);
+    Process process;
+    for (int i = 0; i < scheduler.numP1; i++)
+    {
+        process = scheduler.priority1[i];
+        printProcess(process);
+    }
+
+    printf("Priority2 (%d):\n---------------------\n", scheduler.numP2);
+    for (int i = 0; i < scheduler.numP2; i++)
+    {
+        process = scheduler.priority2[i];
+        printProcess(process);
+    }
+
+    printf("Priority3 (%d):\n---------------------\n", scheduler.numP3);
+    for (int i = 0; i < scheduler.numP3; i++)
+    {
+        process = scheduler.priority3[i];
+        printProcess(process);
+    }
+}
+
+#endif /* UTIL_H */
